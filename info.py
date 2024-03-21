24a3a9981e1959dbe115a714d3b89872f90b4203
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-03-21 19:40:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,13 +96,13 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://thalapathy-2-fol3.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://thalapathy-2-fol3.onrender.com/".format(FQDN, PORT)
+URL = "https://autofuilter-62528a80ce4b.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://autofuilter-62528a80ce4b.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'autofuilter'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'LazyPrincess'))
+name = str(environ.get('name', 'autofuilter'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://thalapathy-2-fol3.onrender.com/".format(FQDN)
+    URL = "https://autofuilter-62528a80ce4b.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://thalapathy-2-fol3.onrender.com/".format(FQDN)
+    URL = "https://autofuilter-62528a80ce4b.herokuapp.com/".format(FQDN)
 
 
 
